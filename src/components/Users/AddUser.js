869fc7c360f8a0512655092830286bde11efedd8
Stephen Kettley <kettleystephen@gmail.com>
2023-11-06 15:15:04 +0200

    setup user form

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
